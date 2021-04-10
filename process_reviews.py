4d7a6499a790d6b915b7e10012ce2de58e4c9db9
Emily Pilley <emilympilley@gmail.com>
2021-04-09 22:31:19 -0400

    save models

--- process_reviews.py
+++ process_reviews.py
@@ -9,8 +9,10 @@ import sys
 import spacy
 import numpy as np
 import pandas as pd
+import joblib
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import NMF
+from sklearn.metrics import silhouette_score
 from sklearn.decomposition import LatentDirichletAllocation as LDA
 
 
@@ -130,36 +132,86 @@ class GoodreadsBookInfo():
 class BookTextAnalyzer():
     def __init__(self, synopses_list, reviews_list, seed=7):
         self.seed = seed
-        self.synopses_list = synopses_list
-        self.reviews_list = reviews_list
         self.spacy_nlp = spacy.load('en_core_web_sm')
 
+        self.synopses_list = synopses_list
+        self.reviews_list = reviews_list
+        
+        self.synopses_topics = self.get_synopses_topics()
+        self.reviews_topics = self.get_reviews_topics()
 
-    def get_synopsis_topics(self, num_topics=20):
+    def get_text_features(self, text_list):
+        processed_text = []
+
         # lemmatize, remove non-alphabetic words and stopwords
-        processed_synopses = []
-        for synopsis in self.spacy_nlp.pipe(self.synopses_list):
-            only_alpha = ' '.join(token.lemma_ for token in synopsis if token.lemma_.isalpha() and not token.is_stop)
-            processed_synopses.append(only_alpha)
+        for text in self.spacy_nlp.pipe(text_list):
+            only_alpha = ' '.join(token.lemma_ for token in text if token.lemma_.isalpha() and not token.is_stop)
+            processed_text.append(only_alpha)
         
         tf_idf_vec = TfidfVectorizer(max_features=5000, lowercase=True, tokenizer=self.spacy_nlp)
-        synopses_features = tf_idf_vec.fit_transform(processed_synopses)
 
-        nmf = NMF(n_components=num_topics, random_state=self.seed)
-        nmf.fit(synopses_features)
-
+        text_features = tf_idf_vec.fit_transform(processed_text)
         # list of unique words found by vectorizer
-        synopses_feature_names = tf_idf_vec.get_feature_names()
+        text_feature_names = tf_idf_vec.get_feature_names()
 
-        # number of words to display per topic
-        n_top_words = 10
+        return (text_features, text_feature_names)
+    
 
+    def get_synopses_topic_model(self, text_features, num_topics=20):
+        if path.exists('topic_models/synopses_model.joblib'):
+            with open('topic_models/synopses_model.joblib', 'rb') as f:
+                return joblib.load('topic_models/synopses_model.joblib')
+        else:
+            nmf = NMF(n_components=num_topics, random_state=self.seed)
+            nmf.fit(text_features)
+            joblib.dump(nmf, 'topic_models/synopses_model.joblib')
+            return nmf
+    
+    def get_reviews_topic_model(self, text_features, num_topics=20):
+        if path.exists('topic_models/reviews_model.joblib'):
+            with open('topic_models/reviews_model.joblib', 'rb') as f:
+                return joblib.load('topic_models/reviews_model.joblib')
+        else:
+            nmf = NMF(n_components=num_topics, random_state=self.seed)
+            nmf.fit(text_features)
+            joblib.dump(nmf, 'topic_models/reviews_model.joblib')
+            return nmf
+
+
+    def get_clusters(self, text_list, for_synopses=False, num_topics=20, words_per_topic=10):
+        text_features, text_feature_names = self.get_text_features(text_list)
+
+        if for_synopses:
+            nmf = self.get_synopses_topic_model(text_features, num_topics)
+        else:
+            nmf = self.get_reviews_topic_model(text_features, num_topics)
+
+        topic_clusters = []
         for idx, topic_vec in enumerate(nmf.components_):
             print(idx, end=' ')
-            for fid in topic_vec.argsort()[-1:-n_top_words-1:-1]:
-                print(synopses_feature_names[fid], end=' ')
-            print()
 
+            topic_words = []
+            for fid in topic_vec.argsort()[-1:-words_per_topic-1:-1]:
+                print(text_feature_names[fid], end=' ')
+                topic_words.append(text_feature_names[fid])
+
+            print()
+            topic_clusters.append({str(idx) : topic_words})
+        
+        return topic_clusters
+    
+
+    def get_synopses_topics(self, num_topics=20, words_per_topic=10):
+        print('SYNOPSES TOPICS:')
+        return self.get_clusters(self.synopses_list, for_synopses=True, 
+                num_topics=num_topics, words_per_topic=words_per_topic)
+    
+
+    def get_reviews_topics(self, num_topics=20, words_per_topic=10):
+        print('REVIEWS TOPICS:')
+        return self.get_clusters(self.reviews_list, for_synopses=False, 
+                num_topics=num_topics, words_per_topic=words_per_topic)
+    
 
 if __name__ == '__main__':
     max_rec = 0x100000
@@ -176,4 +228,8 @@ if __name__ == '__main__':
     # print(all_reviews[77])
 
     book_analyzer = BookTextAnalyzer(all_synopses, all_reviews)
-    book_analyzer.get_synopsis_topics()
+    # print('SYNOPSES TOPICS:')
+    # book_analyzer.get_synopses_topics()
+
+    # print('REVIEWS TOPICS:')
+    # book_analyzer.get_reviews_topics()
